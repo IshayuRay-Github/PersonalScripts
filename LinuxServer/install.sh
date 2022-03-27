70981bbdbddaa03f625eef4c7b4a56d435a0a295
IshayuRay-Github <82944705+IshayuRay-Github@users.noreply.github.com>
2022-03-27 15:23:08 +0530

    Update install.sh

--- LinuxServer/install.sh
+++ LinuxServer/install.sh
@@ -1,2 +1,4 @@
 sudo apt-get install apache2
 sudo apt-get install openjdk-17-jre
+wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+java -Xmx1024M -Xms1024M -jar server.jar nogui
